4e631efa0818763ff83a6710cde0ca734bd6a08b
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2023-05-27 07:20:40 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -69,3 +69,51 @@
 
 // ↑↑ 有料コンテンツ用アナウンス ↑↑ ---------------------- 
 
+
+
+// ↓↓ サムネイル複製 ↓↓ ---------------------- 
+
+  const personlist = document.getElementById("personlist");
+
+  function thumb_clone(){
+    
+    for(let i = 0;i < (persons.length - 1);i++){
+      let clone = personlist.firstElementChild.cloneNode(true);
+      personlist.appendChild(clone);
+    };
+
+  };
+
+// ↑↑ サムネイル複製 ↑↑ ---------------------- 
+
+
+// ↓↓ サムネイル表示 ↓↓ ---------------------- 
+
+  const thumbnail_img = document.getElementsByClassName("thumbnail_img");
+  const thumbnail_info = document.getElementsByClassName("thumbnail_info");
+  const thumbnail_oneword = document.getElementsByClassName("thumbnail_oneword");
+  const prof_link = document.getElementsByClassName("prof_link");
+
+  function thumb_apear(){
+
+    for(let i = 0;i < thumbnail_img.length;i++){
+      thumbnail_img[i].style.backgroundImage = `url(${persons[i].img})`;
+      thumbnail_info[i].innerHTML = `${persons[i].age}歳/${persons[i].place}/${persons[i].name}`;
+      thumbnail_oneword[i].innerHTML = persons[i].oneword;
+      prof_link[i].href = `profile.html?p=${i}`;
+    };
+
+  };
+  
+  // ↑↑ サムネイル表示 ↑↑ ---------------------- 
+
+  new Promise((resolve)=>{
+
+    thumb_clone();
+    resolve();
+
+  }).then(()=>{
+
+    thumb_apear()
+    
+  });
--- js/prof.js
+++ js/prof.js
@@ -25,7 +25,7 @@
 
   const favorite = document.getElementById("favorite");
   const love = document.getElementById("love");
-  const prof_img = document.getElementsByClassName("prof_mainimag");
+  // const prof_img = document.getElementsByClassName("prof_mainimag");
 
   let love_current = false;
 
@@ -41,6 +41,82 @@
     };
   };
   favorite.addEventListener('click',heartfull);
-  prof_img[0].addEventListener('click',heartfull);
+  love.addEventListener('click',heartfull);
 
 // ↑↑ ハートアニメーション ↑↑ ------------------
+
+
+// ↓↓ 有料コンテンツ用アナウンス ↓↓ ---------------------- 
+
+const attention = document.getElementById("attention");
+const paid_contents = document.getElementsByClassName("paid_content");
+
+
+for(let i = 0;i < paid_contents.length;i++){
+  paid_contents[i].addEventListener('click',()=>{
+    attention.classList.remove("hidden");
+  });
+};
+
+attention.addEventListener('click',()=>{
+  attention.classList.add("hidden");
+});
+
+// ↑↑ 有料コンテンツ用アナウンス ↑↑ ---------------------- 
+
+
+// ↓↓ プロフ入力 ↓↓ ------------------
+
+
+  top_name.innerHTML = persons[getParam("p")].name;
+  prof_name.innerHTML = persons[getParam("p")].name;
+  mainimg.style.backgroundImage = `url(${persons[getParam("p")].img})`;
+  oneword.innerHTML = persons[getParam("p")].oneword;
+  age_place.innerHTML = `${persons[getParam("p")].age}歳/${persons[getParam("p")].place}`;
+  prof_name.innerHTML = persons[getParam("p")].name;
+  introduction.innerHTML = persons[getParam("p")].introduction;
+  height.innerHTML = `${persons[getParam("p")].height}cm`;
+  figure.innerHTML = persons[getParam("p")].figure;
+  alcohol.innerHTML = persons[getParam("p")].alcohol;
+  smoking.innerHTML = persons[getParam("p")].smoking;
+  work.innerHTML = persons[getParam("p")].work;
+  wish.innerHTML = persons[getParam("p")].wish;
+  
+
+// ↑↑ プロフ入力 ↑↑ ------------------
+
+
+// ↓↓ 矢印で遷移 ↓↓ ------------------
+
+  const arrow_back = document.getElementById("arrow_back");
+  const arrow_forward = document.getElementById("arrow_forward");
+
+  let person_current = Number(getParam("p"));
+
+  let person_back = person_current - 1;
+  let person_forward = person_current + 1;
+
+  if(person_back < 0){
+    person_back = persons.length - 1;
+  };
+  if(person_forward > (persons.length - 1) ){
+    person_forward = 0;
+  };
+
+  arrow_back.href = `profile.html?p=${person_back}`;
+  arrow_forward.href = `profile.html?p=${person_forward}`;
+
+// ↑↑ 矢印で遷移 ↑↑ ------------------
+
+
+// ↓↓ 勝也の場合のみdtの変更 ↓↓ ------------------
+
+  const work_dt = document.getElementById("work_dt");
+  const wish_dt = document.getElementById("wish_dt");
+
+  if(person_current == (persons.length - 1)){
+    work_dt.innerHTML = "年収"
+    wish_dt.innerHTML = "資産"
+  };
+
+// ↑↑ 勝也の場合のみdtの変更 ↑↑ ------------------
